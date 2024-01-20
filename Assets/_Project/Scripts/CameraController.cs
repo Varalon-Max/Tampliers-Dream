b22c78a27711c6836dbf611e468c04c03bddaa92
valerii.korniienko <vakor.dev@gmail.com>
2024-01-20 13:18:18 +0200

    CameraController finished

---
+++ Assets/_Project/Scripts/CameraController.cs
@@ -0,0 +1,129 @@
+﻿using _Project.Scripts.Configs;
+using Cinemachine;
+using UnityEngine;
+
+namespace _Project.Scripts
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private CameraControllerConfigSO config;
+        [SerializeField] private Transform target;
+        [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+        
+        [SerializeField] private bool useEdgeScrolling;
+
+        private bool _dragPanMoveActive;
+        private Vector2 _lastMousePosition;
+
+        private void Update()
+        {
+            HandleMovement();
+            HandleRotation();
+            HandleZoom();
+        }
+
+        private void HandleZoom()
+        {
+            float targetFov = cinemachineVirtualCamera.m_Lens.FieldOfView;
+
+            float zoom = 0f;
+            if (Input.mouseScrollDelta.y > 0)
+            {
+                zoom = -1f;
+            }
+            else if (Input.mouseScrollDelta.y < 0)
+            {
+                zoom = 1f;
+            }
+
+            targetFov += zoom * config.zoomDistance;
+            targetFov = Mathf.Clamp(targetFov, config.minFov, config.maxFov);
+
+            cinemachineVirtualCamera.m_Lens.FieldOfView = Mathf.Lerp(cinemachineVirtualCamera.m_Lens.FieldOfView,
+                targetFov,
+                Time.deltaTime * config.zoomSpeed);
+        }
+
+        private void HandleRotation()
+        {
+            float rotation = 0f;
+            if (Input.GetKey(KeyCode.Q))
+            {
+                rotation = 1f;
+            }
+            else if (Input.GetKey(KeyCode.E))
+            {
+                rotation = -1f;
+            }
+
+            RotateTarget(rotation);
+        }
+
+        private void HandleMovement()
+        {
+            var horizontal = Input.GetAxis("Horizontal");
+            var vertical = Input.GetAxis("Vertical");
+
+            if (useEdgeScrolling)
+            {
+                ReadEdgeScrollingInput(ref horizontal, ref vertical);
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                _dragPanMoveActive = true;
+
+                _lastMousePosition = Input.mousePosition;
+            }
+
+            if (Input.GetMouseButtonUp(1))
+            {
+                _dragPanMoveActive = false;
+            }
+
+            if (_dragPanMoveActive)
+            {
+                var delta = (Vector2)Input.mousePosition - _lastMousePosition;
+
+                _lastMousePosition = Input.mousePosition;
+                horizontal = -delta.x * config.dragPanSpeedModifier;
+                vertical = -delta.y * config.dragPanSpeedModifier;
+            }
+
+            MoveTarget(horizontal, vertical);
+        }
+
+        private void ReadEdgeScrollingInput(ref float horizontal, ref float vertical)
+        {
+            if (Input.mousePosition.x < config.edgeScrollingBorderThickness)
+            {
+                horizontal = -1f;
+            }
+            else if (Input.mousePosition.x > Screen.width - config.edgeScrollingBorderThickness)
+            {
+                horizontal = 1f;
+            }
+
+            if (Input.mousePosition.y < config.edgeScrollingBorderThickness)
+            {
+                vertical = -1f;
+            }
+            else if (Input.mousePosition.y > Screen.height - config.edgeScrollingBorderThickness)
+            {
+                vertical = 1f;
+            }
+        }
+
+        private void MoveTarget(float horizontal, float vertical)
+        {
+            target.transform.position += target.transform.forward * (vertical * config.speed * Time.deltaTime);
+            target.transform.position += target.transform.right * (horizontal * config.speed * Time.deltaTime);
+        }
+
+        private void RotateTarget(float rotation)
+        {
+            target.transform.RotateAround(target.position, Vector3.up,
+                rotation * config.rotationSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Configs/CameraControllerConfigSO.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace _Project.Scripts.Configs
+{
+    [CreateAssetMenu(menuName = "Configs/CameraControllerConfig", fileName = "CameraControllerConfig", order = 0)]
+    public class CameraControllerConfigSO : ScriptableObject
+    {
+        [Header("Camera Movement")]
+        public float speed = 10f;
+        public float edgeScrollingBorderThickness = 20f;
+        public float dragPanSpeedModifier = 0.05f;
+        [Header("Camera Rotation")]
+        public float rotationSpeed = 100f;
+        [Header("Camera Zoom")]
+        public float zoomDistance = 5f;
+        public float zoomSpeed = 50f;
+        public float minFov = 15f;
+        public float maxFov = 90f;
+    }
+}
